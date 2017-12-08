86a20faf35a5ead1cf03ae258780e2df20530dd9
goran <gogovoinovski@gmail.com>
2017-12-08 18:47:14 +0100

    gotova

--- app/src/main/java/com/example/goran/myapplication/MyAdapter.java
+++ app/src/main/java/com/example/goran/myapplication/MyAdapter.java
@@ -8,7 +8,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -52,6 +55,19 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
 
         holder.tekst.setText(users.getUsername());
+        holder.name.setText(users.getName());
+        holder.lastname.setText(users.getLastname());
+
+
+        if (users.gender == 'M'){
+
+            Picasso.with(context).load("http://www.hjakober.ch/wp-content/uploads/nobody_m_1024x1024.jpg").centerInside().fit().into(holder.slika);
+
+        }else {
+
+            Picasso.with(context).load("http://fie-conference.org/sites/fie-conference.org/files/styles/large/public/placeholder_female1.jpg?itok=PmIExjHS").centerInside().fit().into(holder.slika);
+
+        }
 
         holder.tekst2.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,8 +95,14 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     public class ViewHolder extends RecyclerView.ViewHolder {
         @BindView(R.id.textv)
         TextView tekst;
+        @BindView(R.id.ime)
+        TextView name;
+        @BindView(R.id.prezime)
+        TextView lastname;
         @BindView(R.id.textv2)
         Button tekst2;
+        @BindView(R.id.pic)
+        ImageView slika;
 
         public ViewHolder(View itemView) {
 
